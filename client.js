964b07a3039f11b7053e9eb66867411fe1d7fed5
SaraKate51115 <sarakate51115@gmail.com>
2020-11-04 22:34:01 +0000

    refactored play.js, registered event handler for the connect event, sent 'Name:' string via TCP

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  //print message when connection established
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  //send up name message once connected (write data to a connection)
+  conn.on('connect', () => {
+    conn.write('Name: SKS');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Message from client: ', data)
+  });
+
+  return conn;
+}
+
+module.exports = { connect }
